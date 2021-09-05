e364683cafff7b3e009953b023466f8268b422ab
Fazal E Rabbi <fazal.rabbi@cloudways.com>
2021-09-05 22:33:23 +0500

    Allow suer to delete the tranaction

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -1,15 +1,21 @@
-import { FC } from 'react'
+import {FC, useContext} from 'react'
 import { ETransactionType, ITransaction } from '../interfaces/ITransaction'
+import GlobalContext from "../context/GlobalContext";
 
 export const Transaction: FC<ITransaction> = ( transaction: ITransaction) => {
-
+    const { expenseDispatch } = useContext(GlobalContext)
     const className = (transaction.type === ETransactionType.CR) ? 'plus' : 'minus'
-
+    const deleteTransaction = (id: number) => {
+        expenseDispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
     return (
         <li className={className}>
             {transaction.description}
             <span>{Math.abs(transaction.value)}</span>
-            <button className="delete-btn">
+            <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>
                 X
             </button>
         </li>
